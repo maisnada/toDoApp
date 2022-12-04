2315ac913b1a0d015a45fb57a47794c828fb0283
Marcelo Palmito <maisnada@gmail.com>
2022-12-03 23:16:21 -0300

    form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,64 @@
-const form = document.querySelector('form');
+"use strict";
 
+const atividade = document.querySelector("#atividade");
+
+const data = document.querySelector("#data");
+
+const horario = document.querySelector("#horario");
+
+const btnSalvar = document.querySelector("#salvar");
+
+const listaDeAtividades = document.querySelector(".lista");
+
+btnSalvar.addEventListener("click", handleSalvar);
+
+definirData();
+
+function handleSalvar() {
+  let descricao = atividade.value;
+
+  let horarioAtividade = horario.value ? horario.value : definirhorarioAtual();
+
+  let item = criarItem(
+    `${formatarData(data.value)} | ${horarioAtividade} | ${descricao}`
+  );
+
+  listaDeAtividades.appendChild(item);
+}
+
+function criarItem(conteudo) {
+  let item = document.createElement("li");
+
+  item.innerText = conteudo.trim();
+
+  item.classList.add("list-group-item", "item");
+
+  return item;
+}
+
+function definirData() {
+  let dataAtual = new Date();
+
+  let dia = String(dataAtual.getDate()).padStart(2, "0");
+  let mes = dataAtual.getMonth() + 1;
+  let ano = dataAtual.getFullYear();
+
+  console.log("6".padStart(2, "0"));
+
+  data.value = `${ano}-${mes}-${dia}`;
+}
+
+function formatarData(data) {
+  let dataArr = data.split("-");
+
+  return `${dataArr[2]}/${dataArr[1]}`;
+}
+
+function definirhorarioAtual() {
+  let dataAtual = new Date();
+
+  let hora = String(dataAtual.getHours()).padStart(2, "0");
+  let minutos = String(dataAtual.getMinutes()).padStart(2, "0");
+
+  return `${hora}h${minutos}`;
+}
